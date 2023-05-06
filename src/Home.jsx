b0a709c0adc253f5fd19a72061e797addb734c5b
VincentNdegwa <116282941+VincentNdegwa@users.noreply.github.com>
2023-05-06 19:55:10 +0300

    update

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,50 +10,50 @@ function Main() {
   const [currentData, setCurrentData] = React.useState(CurrentData);
   const [userInput, setUserInput] = React.useState("Nairobi");
   const [failedFetch, setFailedFetch] = React.useState(false);
-  // React.useEffect(() => {
-  //   setForeCastData(ForeData?.list);
-  //   setCurrentData(CurrentData);
-  // }, []);
   React.useEffect(() => {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=1be21ce2f51b54b9644249a8d1817e6a&units=metric`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.cod === "404") {
-          setUserInput("Nairobi");
-          console.log("city not availlable");
-        } else if (data.cod === "200") {
-          console.log("success");
-          setForeCastData(data?.list);
-        } else {
-          console.log("not successfull");
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    setForeCastData(ForeData?.list);
+    setCurrentData(CurrentData);
+  }, []);
+  // React.useEffect(() => {
+  //   fetch(
+  //     `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=1be21ce2f51b54b9644249a8d1817e6a&units=metric`
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       if (data.cod === "404") {
+  //         setUserInput("Nairobi");
+  //         console.log("city not availlable");
+  //       } else if (data.cod === "200") {
+  //         console.log("success");
+  //         setForeCastData(data?.list);
+  //       } else {
+  //         console.log("not successfull");
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
 
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=1be21ce2f51b54b9644249a8d1817e6a&units=metric`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.cod === "404") {
-          setUserInput("Nairobi");
-          console.log("city not availlable");
-        } else if (data.cod === 200) {
-          console.log("success");
-          setCurrentData(data);
-        } else {
-          console.log("not successfull");
-        }
-      })
-      .catch((err) => {
-        setFailedFetch(true);
-        console.log(err);
-      });
-  }, [userInput]);
+  //   fetch(
+  //     `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=1be21ce2f51b54b9644249a8d1817e6a&units=metric`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data.cod === "404") {
+  //         setUserInput("Nairobi");
+  //         console.log("city not availlable");
+  //       } else if (data.cod === 200) {
+  //         console.log("success");
+  //         setCurrentData(data);
+  //       } else {
+  //         console.log("not successfull");
+  //       }
+  //     })
+  //     .catch((err) => {
+  //       setFailedFetch(true);
+  //       console.log(err);
+  //     });
+  // }, [userInput]);
 
   return (
     <div className="home">
